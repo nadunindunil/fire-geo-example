191084813b8a30952ead1d56141a35262053cb39
Nadun Indunil <nadun1indunil@gmail.com>
2018-03-05 12:41:05 +0530

    prod added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB5u8OSZNYvV9GU49sYIQJERckAZcb1vTg",
+    authDomain: "geo-fire-982d9.firebaseapp.com",
+    databaseURL: "https://geo-fire-982d9.firebaseio.com",
+    projectId: "geo-fire-982d9",
+    storageBucket: "",
+    messagingSenderId: "371532785963"
+  },
+
+  googleMapsKey: 'AIzaSyDwn0wpDXIFhIBubKlrekYq_JrWWopKC34'
 };
